11f7b3199cff74fc24380cdf9cfe56dc47dff00a
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-11-01 15:11:37 +0100

    legend for images

--- upload.php
+++ upload.php
@@ -8,46 +8,49 @@
 
 <?php
 
-    session_start();
-    if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
-        header('Location: login.php'); // Redirect to the login page if not authenticated
-        exit;
-    }
+session_start();
+if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
+    header('Location: login.php'); // Redirect to the login page if not authenticated
+    exit;
+}
 
-    // Check if the form was submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $upload_dir = "images/"; // Directory where you want to store uploaded images
     $uploaded_file = $upload_dir . basename($_FILES['image']['name']);
+    $legend = $_POST['legend']; // Get the legend from the form
 
     if (move_uploaded_file($_FILES['image']['tmp_name'], $uploaded_file)) {
-        // Image uploaded successfully, now insert the image information into the database
+        // Image uploaded successfully
         $imageFilename = basename($_FILES['image']['name']);
-        $uploadedBy = isset($_SESSION['username']) ? $_SESSION['username'] : ''; // Assuming you have a username in the session
-        
-        // Connect to the MySQL database (adjust the connection details as per your configuration)
+        $uploadedBy = $_SESSION['username'];
+
+        // Connect to the MySQL database
         $conn = new mysqli("localhost", "root", "root", "img_upload");
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        // Insert image information into the database
-        $sql = "INSERT INTO images (filename, uploaded_by) VALUES (?, ?)";
+        // Insert image information and legend into the database
+        $sql = "INSERT INTO images (filename, uploaded_by, legend) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $imageFilename, $uploadedBy);
+        $stmt->bind_param("sss", $imageFilename, $uploadedBy, $legend);
 
         if ($stmt->execute()) {
             echo "Image uploaded and information stored in the database!";
         } else {
-            header("HTTP/1.1 500 Internal Server Error"); // Set a 500 status code for the error
-            echo "Image upload failed. Please try again.";
+            header("HTTP/1.1 500 Internal Server Error");
+            echo "Image uploaded, but failed to store information in the database. Please try again.";
         }
 
         $stmt->close();
         $conn->close();
     } else {
+        header("HTTP/1.1 500 Internal Server Error");
         echo "Image upload failed. Please try again.";
     }
 }
+
 ?>
 
     <header>
@@ -56,13 +59,16 @@
 
     <!-- Form for uploading images -->
     <div id="upload-container">
-    <form id="image-upload-form" method="post" enctype="multipart/form-data" onsubmit="uploadImage(event);">
-        <label for="image">Select an image to upload:</label>
-        <input type="file" name="image" id="image" accept="image/*" required>
-        <button type="submit">Upload Image</button>
-    </form>
-    <div id="status-message"></div>
-</div>
+        <form id="image-upload-form" method="post" enctype="multipart/form-data" onsubmit="uploadImage(event);">
+            <label for="image">Select an image to upload:</label>
+            <input type="file" name="image" id="image" accept="image/*" required>
+            <label for="legend">Image Legend:</label>
+            <input type="text" name="legend" id="legend" required maxlength="255">
+            <button type="submit">Upload Image</button>
+        </form>
+        <div id="status-message"></div>
+    </div>
+
 
 
     <footer>
@@ -70,6 +76,7 @@
     </footer>
 
     <script src="js/AJAXForm.js">
+    <script src="js/maxLength.js">
 
 </body>
 </html>
