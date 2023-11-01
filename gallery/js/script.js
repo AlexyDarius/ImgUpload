9a77821b11c0642af31642abd11579f22124ca5e
Roman Alexy <alexy.roman@alumni.enac.fr>
2023-11-01 19:24:45 +0100

    img max size

--- gallery/js/script.js
+++ gallery/js/script.js
@@ -2,6 +2,27 @@ function uploadImage(event) {
     event.preventDefault(); // Prevent the form from being submitted normally
 
     let form = document.getElementById('image-upload-form');
+    let legendInput = document.getElementById('legend');
+    let errorMessage = document.getElementById('error-message');
+
+    if (legendInput.value.length > 255) {
+        errorMessage.textContent = "Legend is too long. Please keep it under 255 characters.";
+        errorMessage.style.display = 'block'; // Display the error message
+        return;
+    }
+
+    // Check the file size
+    let maxFileSize = 100 * 1024; // 100KB in bytes
+    if (document.getElementById('image').files[0].size > maxFileSize) {
+        errorMessage.textContent = "File size exceeds the maximum allowed size (100KB).";
+        errorMessage.style.display = 'block'; // Display the error message
+        return;
+    }
+
+    // Reset error message if no errors
+    errorMessage.style.display = 'none';
+
+    //AJAX request
     let formData = new FormData(form);
 
     // Create an XMLHttpRequest object
--- gallery/upload.php
+++ gallery/upload.php
@@ -18,36 +18,44 @@ if (!isset($_SESSION['authenticated']) || !$_SESSION['authenticated']) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $upload_dir = "images/"; // Directory where you want to store uploaded images
     $uploaded_file = $upload_dir . basename($_FILES['image']['name']);
-    $legend = $_POST['legend']; // Get the legend from the form
+    $legend = $_POST['legend'];
+    $max_file_size = 100000; // 100kb
 
-    if (move_uploaded_file($_FILES['image']['tmp_name'], $uploaded_file)) {
-        // Image uploaded successfully
-        $imageFilename = basename($_FILES['image']['name']);
-        $uploadedBy = $_SESSION['username'];
+    // Check the file size
+   if ($_FILES['image']['size'] > $max_file_size) {
+    header("HTTP/1.1 400 Bad Request");
+    echo "File size exceeds the maximum allowed size (100KB).";
+    }else{
 
-        // Connect to the MySQL database
-        $conn = new mysqli("localhost", "root", "root", "img_upload");
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $uploaded_file)) {
+            // Image uploaded successfully
+            $imageFilename = basename($_FILES['image']['name']);
+            $uploadedBy = $_SESSION['username'];
 
-        // Insert image information and legend into the database
-        $sql = "INSERT INTO images (filename, uploaded_by, legend) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $imageFilename, $uploadedBy, $legend);
+            // Connect to the MySQL database
+            $conn = new mysqli("localhost", "root", "root", "img_upload");
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-        if ($stmt->execute()) {
-            echo "Image uploaded and information stored in the database!";
+            // Insert image information and legend into the database
+            $sql = "INSERT INTO images (filename, uploaded_by, legend) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $imageFilename, $uploadedBy, $legend);
+
+            if ($stmt->execute()) {
+                echo "Image uploaded and information stored in the database!";
+            } else {
+                header("HTTP/1.1 500 Internal Server Error");
+                echo "Image uploaded, but failed to store information in the database. Please try again.";
+            }
+
+            $stmt->close();
+            $conn->close();
         } else {
             header("HTTP/1.1 500 Internal Server Error");
-            echo "Image uploaded, but failed to store information in the database. Please try again.";
+            echo "Image upload failed. Please try again.";
         }
-
-        $stmt->close();
-        $conn->close();
-    } else {
-        header("HTTP/1.1 500 Internal Server Error");
-        echo "Image upload failed. Please try again.";
     }
 }
 
@@ -62,9 +70,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         <form id="image-upload-form" method="post" enctype="multipart/form-data" onsubmit="uploadImage(event);">
             <label for="image">Select an image to upload:</label>
             <input type="file" name="image" id="image" accept="image/*" required>
+            <input type="hidden" name="MAX_FILE_SIZE" value="100000"> <!-- 100ko -->
             <label for="legend">Image Legend:</label>
             <input type="text" name="legend" id="legend" required maxlength="255">
             <button type="submit">Upload Image</button>
+            <div id="error-message" style="color: red; display: none;"></div>
         </form>
         <div id="status-message"></div>
     </div>
@@ -76,7 +86,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     </footer>
 
     <script src="js/AJAXForm.js">
-    <script src="js/maxLength.js">
 
 </body>
 </html>
